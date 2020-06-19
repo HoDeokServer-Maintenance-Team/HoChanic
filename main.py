b325c835304f0e5f9d433f8e0dac7d51b7c7e89b
eunwoo1104 <sions04@naver.com>
2020-06-19 17:13:28 +0900

    Update main.py

--- main.py
+++ main.py
@@ -15,4 +15,22 @@ async def on_ready():
     print("Bot Ready.")
 
 
+@bot.command(name="확인")
+async def _check_ko(ctx):
+    tgt_role = ctx.guild.get_role(723438324777353226)
+    if tgt_role in ctx.author.roles:
+        return await ctx.send("❌ 이미 역할을 받으셨습니다.")
+    await ctx.send("✅ 호덕 디스코드 서버에 오신 것을 환영합니다!")
+    await ctx.author.add_roles(tgt_role)
+
+
+@bot.command(name="confirm", aliases=["check"])
+async def _check_en(ctx):
+    tgt_role = ctx.guild.get_role(721002110883201184)
+    if tgt_role in ctx.author.roles:
+        return await ctx.send("❌ You already got the role.")
+    await ctx.send("✅ Welcome to HoDeok Discord Server!")
+    await ctx.author.add_roles(tgt_role)
+
+
 bot.run(loop.run_until_complete(hochanic_db.get_from_table("bot_settings", "key", "token"))[0][1])
